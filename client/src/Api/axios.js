fcceea075ba5ab254fb7f361cb1f5cc2a44213f2
Eyobed-Tezera <eyobed014@gmail.com>
2024-10-20 05:47:36 -0700

    deployed backend

--- client/src/Api/axios.js
+++ client/src/Api/axios.js
@@ -1,5 +1,11 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "https://evangadi-forum-back-end-753k.onrender.com/api",
+  // local version
+
+  // baseURL: "https://evangadi-forum-back-end-753k.onrender.com/api",
+
+  //deployed version of evangadi-server on render.com
+
+  baseURL: "https://evangadi-forum-2024-2.onrender.com",
 });
 export default instance;
